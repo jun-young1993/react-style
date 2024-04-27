1cb02432109f333cae3236b224fb4e1689997100
junyoungkim <juny3738@gmail.com>
2024-04-27 12:39:03 +0900

    v1.0.3

--- jest.config.ts
+++ jest.config.ts
@@ -4,12 +4,9 @@ const config: Config = {
 	testEnvironment: "jsdom",
 	verbose: true,
 	setupFilesAfterEnv: [
-		"@testing-library/jest-dom"
-		// '<rootDir>/jest-setup.ts'
+		// "@testing-library/jest-dom"
+		'<rootDir>/jest-setup.ts'
 	],
-	// transformIgnorePatterns: [
-	// 	"node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|dom)",	
-	// ]
 }
 
 export default config;
