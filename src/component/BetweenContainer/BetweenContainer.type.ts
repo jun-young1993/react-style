3d9c51de87f2514637cdab4edd0eec842ec057e4
jun-young1993 <juny3738@gmail.com>
2024-06-07 18:11:39 +0900

    v

--- src/component/BetweenContainer/BetweenContainer.type.ts
+++ src/component/BetweenContainer/BetweenContainer.type.ts
@@ -1,7 +1,7 @@
 import {ReactNode} from "react";
 export interface SpacerProps {
-    gap: string | undefined
+    gap?: string | undefined
 }
 export interface BetweenContainerProps extends SpacerProps{
-    children: ReactNode | ReactNode[]
+    children?: ReactNode | ReactNode[]
 }
