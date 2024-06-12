404ee4285c1879c3d64a532ecb547cc333cc87f1
jun-young1993 <juny3738@gmail.com>
2024-06-12 15:00:00 +0900

    v1.6.29

--- src/component/TreeList/TreeList.type.ts
+++ src/component/TreeList/TreeList.type.ts
@@ -1,15 +1,15 @@
 import { TextBoxWrapProps } from "../utills/index.type";
 import {ReactNode} from "react";
-export interface TreeItemProps {
+export interface TreeItemProps<T = {[key: string]: any}> {
     active?: any
     title?: string
-    userData?:{[key: string]: any}
+    userData?: T
     textOption?: TextBoxWrapProps
 }
-export interface TreeListProps {
-    onClick?: (item: TreeItemProps) => void
+export interface TreeListProps<T> {
+    onClick?: (item: TreeItemProps<T>) => void
     title?: string | ReactNode
     icon?: ReactNode
-    items?: TreeItemProps[] | [],
+    items?: TreeItemProps<T>[] | [],
 }
 
